0f55585ccdb41d61deb4b602aa724c5e097500cf
hamilton moore <hamlmoor@gmail.com>
2017-09-06 15:34:05 -0500

    adjusted formatting on tests

--- server.js
+++ server.js
@@ -1,85 +1,14 @@
 const app = require("./app");
 const port = process.env.PORT || 8100
-const Item = require("./models/item");
 
-//new item for the db
-app.post("/api/vendor/items", function (req, res) {
-    let newItem = new Item(req.body)
-    newItem
-        .save() //this saves info to the db
-        .then(function (savedItem) {  //this is data passed back from db
-            return res.json({ item: savedItem }) //returning info from db 
-        })
-        .catch(function (err) {
-            console.log("this is err: ", err); //err is the error from the .then method within mongoose
-            return res.status(500).send(err);
-        })
-});
-
-//see list of items
-app.get("/api/customer/items", function (req, res) {
-    Item.find(function (foundItem) {
-        if (!foundItem) {
-            return res.send({ msg: "no Items found" })
-        }
-        return res.json({ item: foundItem })
-    })
-        .catch(function (err) {
-            return res.status(500).send(err);
-        })
-})
-
-//save a purchase
-app.post("/api/customer/items/:itemId/purchases", function (req, res) {
-    let newPurchase = new Purchase(req.body)
-    newPurchase
-        .save()
-        .then(function (savedPurchase) {
-            return res.json({ item: savedPurchase })
-        })
-        .catch(function (err) {
-            return res.status(500).send(err)
-        })
-})
-
-//gets a list of purchases with times
-app.get("/api/vendor/purchases", function (req, res) {
-    Item.find(function (foundPurchases) {
-        if (!foundPurchases) {
-            return res.send({ msg: "no purchases found" })
-        }
-        res.json({ item: foundPurchases })
-    })
-        .catch(function (err) {
-            return res.status(500).send(err)
-        })
-})
-
-app.get("/api/vendor/money", function (req, res) {
-    Item.find(function (foundMoney) {
-        if (!foundMoney) {
-            return res.send({ msg: "no money in machine" })
-        }
-        res.json({ item: foundPurchases })
-    })
-        .catch(function (err) {
-            return res.status(500).send(err)
-        })
-})
-
-//updates item description, quantity and cost
-app.put("/api/vendor/items/:itemId", function (req, res) {
-    Item.findByIdAndUpdate(req.params.id, req.body)
-        .then(function (updatedItem) {
-            if (!updatedItem) {
-                return res.send({ msg: "could not update item" });
-            }
-            res.json({ item: updatedItem })
-        })
-        .catch(function (err) {
-            res.status(500).send(err);
-        });
-});
+//function to add money in machine:
+// function addMoney() {
+//     let sum;
+//     for (i = 0; i < argument.length; i++) {
+//         sum = arguments[i] + 
+//      }
+//     addMoney({ foundItem: foundItem: {item: cost }})
+// }
 
 app.listen(port, function () {
     console.log(`server is running on port ${port}!`);
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,5 +1,6 @@
 const request = require("supertest");
 const app = require("../app");
+const Item = require("../models/item");
 
 const mockup = {
     description: "lays",
@@ -9,15 +10,30 @@ const mockup = {
 
 //this tests the addition of an item
 describe("POST /item", function () {
-    test("should return successfully", function () {
+    test("should create new items in machine", function () {
+        console.log('mockup: ', mockup);
         return request(app)
             .post("/api/vendor/items")  //this is the route we're posting to 
-            .send(item)
-            .expect(response.status).toBe(200)
-            .expect(reponse.body.data.quantity).toBe(5)
-            .expect(response.body.data.description).toBe("lays")
-            .expect(response.body.data.price).toBe(35)
-            .expect(function (res) {
+            .send(mockup)
+            .then(response => {
+                expect(response.status).toBe(200)
+                console.log('response.body: ', response.body);
+                expect(response.body.data.quantity).toBe(5)
+                expect(response.body.data.description).toBe("lays")
+                expect(response.body.data.price).toBe(35)
+            });
+    })
+
+    //this tests the purchase of an item
+    test("should test purchase of an item", function () {
+        return request(app)
+            .post("/api/customer/items/:itemId/purchases")
+            .send(mockup)
+            .then(response => {
+                exepct(response.status).toBe(200)
+                expect(response.body.data.quantity).toBe(5)
+                expect(response.body.data.description).toBe("lays")
+                expect(response.body.data.price).toBe(35)
             });
     })
 })
@@ -27,72 +43,51 @@ describe("GET /item", function () {
     test("should return successfully", function () {
         return request(app)
             .post("/api/customer/items")
-            .send(item)
-            .exepct(response.status).toBe(200)
-            .expect(response.body.data.quantity).toBe(5)
-            .expect(response.body.data.description).toBe("lays")
-            .expect(response.body.data.price).toBe(35)
-            .expect(function (res) {
+            .send(mockup)
+            .then(response => {
+                exepct(response.status).toBe(200)
+                expect(response.body.data.quantity).toBe(5)
+                expect(response.body.data.description).toBe("lays")
+                expect(response.body.data.price).toBe(35)
             });
     })
-})
-
-//this tests the purchase of an item
-describe("POST /item", function () {
-    test("should return successfully", function () {
-        return request(app)
-            .post("/api/customer/items/:itemId/purchases")
-            .send(item)
-            .exepct(response.status).toBe(200)
-            .expect(response.body.data.quantity).toBe(5)
-            .expect(response.body.data.description).toBe("lays")
-            .expect(response.body.data.price).toBe(35)
-            .expect(function (res) {
-            });
-    })
-})
-
-//this tests lists of purchases GET 
-describe("GET /item", function () {
-    test("should return successfully", function () {
+    //this tests lists of purchases GET 
+    test("should display purchases", function (response) {
         return request(app)
             .post("/api/vendor/purchases")
-            .send(item)
-            .expect(response.status).toBe(200)
-            .expect(response.body.data.quantity).toBe(5)
-            .expect(response.body.data.description).toBe("lays")
-            .expect(response.body.data.price).toBe(35)
-            .expect(function (res) {
+            .send(mockup)
+            .then(response => {
+                expect(response.status).toBe(200)
+                expect(response.body.data.quantity).toBe(5)
+                expect(response.body.data.description).toBe("lays")
+                expect(response.body.data.price).toBe(35)
             });
     })
 })
-
 //requests amount of money in machine
-describe("GET /item", function () {
-    test("should return successfully", function () {
-        return request(app)
-            .post("/api/vendor/money")
-            .send(item)
-            .expect(response.status).toBe(200)
-            .expect(response.body.data.quantity).toBe(5)
-            .expect(response.body.data.description).toBe("lays")
-            .expect(response.body.data.price).toBe(35)
-            .expect(function (res) {
-            });
-    })
+test("should display amount of money", function (response) {
+    return request(app)
+        .post("/api/vendor/money")
+        .send(mockup)
+        .then(response => {
+            expect(response.status).toBe(200)
+            expect(response.body.data.quantity).toBe(5)
+            expect(response.body.data.description).toBe("lays")
+            expect(response.body.data.price).toBe(35)
+        });
 })
 
 //this updates an item
 describe("PUT /item", function () {
-    test("should return successfully", function () {
+    test("should return successfully", function (response) {
         return request(app)
             .post("/api/vendor/items/:itemId")
-            .send(item)
-            .expect(response.status).toBe(200)
-            .expect(response.body.data.quantity).toBe(5)
-            .expect(response.body.data.description).toBe("lays")
-            .expect(response.body.data.price).toBe(35)
-            .expect(function (res) {
+            .send(mockup)
+            .then(response => {
+                expect(response.status).toBe(200)
+                expect(response.body.data.quantity).toBe(5)
+                expect(response.body.data.description).toBe("lays")
+                expect(response.body.data.price).toBe(35)
             });
     })
 })
